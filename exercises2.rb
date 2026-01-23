4a0a88238d9922f38126119e0f885a551dfb7a16
Yashrajlad28 <yashrajlad6@gmail.com>
2026-01-23 16:24:20 +0530

    Adding partially completed exercises

--- exercises2.rb
+++ exercises2.rb
@@ -56,6 +56,91 @@ car1.stop
 car2 = Car.new("Hyundai", "Palisade", "black")
 car2.stop
 
+class Animal
+    def initialize
+        puts "Animal class constructor"
+    end
+
+    def eat
+        puts "Animal is now eating"
+    end
+
+    def sleep
+        puts "Animal is sleeping"
+    end
+
+end
+
+class Dog < Animal
+    def initialize
+        puts "Dog class constructor"
+    end
+
+    def make_sound
+        puts "Dog is barking"
+    end
+end
+
+obj_d = Dog.new
+
+obj_d.eat
+obj_d.make_sound
+# obj_a.make_sound
 puts "----------------------------------------------------"
 puts
+
+puts "Module 8"
+
+module Biology
+
+    BIO_CHAPTERS = 5
+
+    def extract_samples
+        puts "Extracting samples"
+    end
+
+    def examine_samples
+        puts "Examining extracted samples under a microscope"
+    end
+
+end
+
+module Chemistry
+
+    CHEM_CHAPTERS = 5
+
+    def perform_experiments
+        puts "Performing experiments in chemistry lab"
+    end
+
+end
+
+module Physics
+    CHEM_CHAPTERS = 6
+
+    def understand_derivations
+        puts "Write and thoroughly understand derivations"
+    end
+
+    def solve_numericals
+        puts "Solving numericals in physics"
+    end
+end
+
+class Science
+    include Biology
+    include Chemistry
+    include Physics
+
+    def initialize
+        puts "Constructor of SCience class"
+    end
+
+    def study
+        puts "To study Science you need to learn Physics, Chemistry and Biology"
+    end
+end
+
+sbj = Science.new
+puts sbj.extract_samples
 
